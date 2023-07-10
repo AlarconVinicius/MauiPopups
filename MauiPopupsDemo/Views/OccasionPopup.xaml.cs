f7a294ce99a528ee05a4eb0ea692b20e9363622b
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-07-10 12:57:50 -0300

    final project

---
+++ MauiPopupsDemo/Views/OccasionPopup.xaml.cs
@@ -0,0 +1,28 @@
+using CommunityToolkit.Maui.Views;
+using MauiPopupsDemo.Models;
+
+namespace MauiPopupsDemo.Views;
+
+public partial class OccasionPopup : Popup
+{
+	public OccasionPopup()
+	{
+		InitializeComponent();
+	}
+
+    private void CancelButton_Clicked(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    private void Button_Clicked_1(object sender, EventArgs e)
+    {
+        OccasionModel occasion = new OccasionModel
+        {
+            Date = OccasionDate.Date,
+            Occasion = OccasionType.SelectedItem.ToString(),
+            Notes = OccasionNotes.Text
+        };
+        Close(occasion);
+    }
+}
